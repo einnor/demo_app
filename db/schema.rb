0cb837181e90df210a6978445dbdd0953e8adc7b
Ronnie Nyaga <rnafundi@gmail.com>
2013-07-28 17:15:55 +0300

    Added a Micropost resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130728132610) do
+ActiveRecord::Schema.define(version: 20130728141403) do
+
+  create_table "microposts", force: true do |t|
+    t.string   "content"
+    t.string   "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
